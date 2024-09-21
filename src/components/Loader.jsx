a1f6c8dad71a8ef32f8994d6335d28736d6debaf
goosedum <154931486+goosedum@users.noreply.github.com>
2024-09-21 22:48:19 +0300

    ff

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,18 +1,20 @@
 import { MagnifyingGlass } from 'react-loader-spinner';
 
-const Loader = () => (
-  <RotatingLines
-    visible={true}
-    height="90"
-    width="90"
-    strokeColor="#0d00c3"
-    strokeWidth="4"
-    animationDuration="0.65"
-    ariaLabel="rotating-lines-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-  />
-);
+const Loader = () => {
+  return (
+    <RotatingLines
+      visible={true}
+      height="90"
+      width="90"
+      strokeColor="#0d00c3"
+      strokeWidth="4"
+      animationDuration="0.65"
+      ariaLabel="rotating-lines-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+  );
+};
 
 
-export default Loader;
+  export default Loader;
