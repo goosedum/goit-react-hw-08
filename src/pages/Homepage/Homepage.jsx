62d5a4a25fce32c140ad8bdeb288eafcf2d81721
goosedum <154931486+goosedum@users.noreply.github.com>
2024-09-21 23:01:50 +0300

    zxcb

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import css from './Homepage.module.css';
 
 const Homepage = () => {
-  return <h1 className={css.pageContainer}>Hi!</h1>;
+  return <h1 className={css.pageContainer}>Hi! It's your Phonebook</h1>;
 };
 
 export default Homepage;
